0d2b1d61b89f318c60f3714284b564551255130f
kohn doe <testing@testing.com>
2020-01-02 08:18:26 -0800

    starting on LinkedList implementation

---
+++ linkedList.js
@@ -0,0 +1,14 @@
+function LinkedList(isSinglyLinked, elementsArray){
+    let _head = null
+    let _getNode = ()=> {return {data:null, next:null}}
+
+    let previousNode
+    for(let i = elementsArray.length - 1;i = 0;i--){
+        if (i == elementsArray.length - 1){
+            
+        }
+        let newNode = _getNode()
+        newNode.data = elementsArray[i]
+        newNode.next = 
+    }
+}
